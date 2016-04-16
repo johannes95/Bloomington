c1985cb5cbcfaf6ff417c48a086f6facc339e2f7
Johannes Flood <johannes_flood@hotmail.com>
2016-04-16 17:22:36 +0200

    trying the example on papers website instead

--- js/skittles.js
+++ js/skittles.js
@@ -1,48 +1,12 @@
 $(document).ready(function(){
-	// Create a raster item using the image tag with id='mona'
-var raster = new Raster('mona');
-
-// Hide the raster:
-raster.visible = false;
-
-// The size of our grid cells:
-var gridSize = 12;
-
-// Space the cells by 120%:
-var spacing = 1.2;
-
-// As the web is asynchronous, we need to wait for the raster to load
-// before we can perform any operation on its pixels.
-raster.on('load', function() {
-	// Since the example image we're using is much too large,
-	// and therefore has way too many pixels, lets downsize it to
-	// 40 pixels wide and 30 pixels high:
-	raster.size = new Size(40, 30);
-
-	for (var y = 0; y < raster.height; y++) {
-		for(var x = 0; x < raster.width; x++) {
-			// Get the color of the pixel:
-			var color = raster.getPixel(x, y);
-
-			// Create a circle shaped path:
-			var path = new Path.Circle({
-				center: new Point(x, y) * gridSize,
-				radius: gridSize / 2 / spacing
-			});
-
-			// Set the fill color of the path to the color
-			// of the pixel:
-			path.fillColor = color;
-		}
-	}
-
-	// Move the active layer to the center of the view, so all 
-	// the created paths in it appear centered.
-	project.activeLayer.position = view.center;
-});
-
-// Move the active layer to the center of the view:
-project.activeLayer.position = view.center;
-
-
+	// Create a Paper.js Path to draw a line into it:
+	var path = new Path();
+	// Give the stroke a color
+	path.strokeColor = 'black';
+	var start = new Point(100, 100);
+	// Move to start and draw a line from there
+	path.moveTo(start);
+	// Note the plus operator on Point objects.
+	// PaperScript does that for us, and much more!
+	path.lineTo(start + [ 100, -50 ]);
 });
