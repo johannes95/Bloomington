af1d17355970f3b448f60a8653fa15e38bea567a
Johannes Flood <johannes_flood@hotmail.com>
2016-04-16 19:13:54 +0200

    Successfully enlarging pixels of image and calculating how many skittles will be required

--- index.php
+++ index.php
@@ -4,21 +4,27 @@
 </head>
 
 <?php 
+	//$skittle_colors = array();
 	$src = "img/stevejobs.jpg";
 	$img = imagecreatefromjpeg($src);
 	$img_size = array("width" => getimagesize($src)[0], "height" => getimagesize($src)[1]);
-	
-	for($h = 0; $h < 50; $h++){
-	for($w = 0; $w < $img_size["width"]; $w++){
+	$output_pixel_size = 10;
+	$skittles_required = 0;
+
+	for($h = 0; $h < $img_size["height"]; $h += $output_pixel_size){
+	for($w = 0; $w < $img_size["width"]; $w += $output_pixel_size){
 		$rgb = imagecolorat($img, $w, $h);
 		$r = ($rgb >> 16) & 0xFF;
 		$g = ($rgb >> 8) & 0xFF;
 		$b = $rgb & 0xFF;
 
-		echo '<div class="p" style="background:rgb('.$r.', '.$g.', '.$b.');"></div>';
+		echo '<div class="p" style="width:'.$output_pixel_size.'px;height:'.$output_pixel_size.'px;left:'.$w.'px;top:'.$h.'px;background:rgb('.$r.', '.$g.', '.$b.');"></div>';
+		$skittles_required++;
 	}
 		echo "<br />";
 	}
+
+	echo '<div id="output">skittles required: '.$skittles_required.'</div>';
 
 	
 ?>
