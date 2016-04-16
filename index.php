ee72923db6d4d101d24d65266703509eb83dcd7c
Johannes Flood <johannes_flood@hotmail.com>
2016-04-16 20:19:26 +0200

    Completed reading image pixels and outputing the closest skittle color

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <head>
 	<link type="text/css" rel="stylesheet" href="css/style.css" />
 </head>
-
+<body>
 <?php 
-	$skittle_colors = array(array(214,24,43,94), array(222,118,27,122));
+	$skittle_colors = array(array(214,24,43,94), array(222,118,27,122), array(250,246,40,175), array(14,199,51,88), array(86, 6, 117, 209), array(42, 108, 232, 127), array(242,148,148,179));
 	$src = "img/stevejobs.jpg";
 	$img = imagecreatefromjpeg($src);
 	$img_size = array("width" => getimagesize($src)[0], "height" => getimagesize($src)[1]);
-	$output_pixel_size = 1;
+	$output_pixel_size = 8;
 	$skittles_required = 0;
 
 	for($h = 0; $h < $img_size["height"]; $h += $output_pixel_size){
@@ -18,15 +18,17 @@
 		$g = ($rgb >> 8) & 0xFF;
 		$b = $rgb & 0xFF;
 
-
 		$avg = ($r+$g+$b) / 3;
-		if($avg > 110){
-			$avg = 255;
-		}else{
-			$avg = 0;
+		$best = array("index" => -1, "diff" => 256);
+		for($y = 0; $y < sizeof($skittle_colors); $y++){
+			$diff = sqrt(pow($avg - $skittle_colors[$y][3], 2));
+			if($diff < $best["diff"]){
+				$best["index"] = $y;
+				$best["diff"] = $diff;
+			}
 		}
 
-		echo '<div class="p" style="width:'.$output_pixel_size.'px;height:'.$output_pixel_size.'px;left:'.$w.'px;top:'.$h.'px;background:rgb('.$avg.', '.$avg.', '.$avg.');"></div>';
+		echo '<div class="p" style="border-radius:'.$output_pixel_size.'px;width:'.$output_pixel_size.'px;height:'.$output_pixel_size.'px;left:'.$w.'px;top:'.$h.'px;background:rgb('.$skittle_colors[$best["index"]][0].', '.$skittle_colors[$best["index"]][1].', '.$skittle_colors[$best["index"]][2].');"></div>';
 		$skittles_required++;
 	}
 		echo "<br />";
@@ -36,3 +38,5 @@
 
 	
 ?>
+</body>
+</html>
