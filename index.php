83cc1b25de1da220df786e4bc88783d079a1486f
Johannes Flood <johannes_flood@hotmail.com>
2016-04-16 18:55:47 +0200

    Reading image pixels and outputing as divs

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
-<?php
-	$im = imagecreatefromjpeg("img/stevejobs.jpg");
-	$rgb = imagecolorat($im, 10, 15);
-	$r = ($rgb >> 16) & 0xFF;
-	$g = ($rgb >> 8) & 0xFF;
-	$b = $rgb & 0xFF;
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+</head>
 
-	var_dump($r, $g, $b);
+<?php 
+	$src = "img/stevejobs.jpg";
+	$img = imagecreatefromjpeg($src);
+	$img_size = array("width" => getimagesize($src)[0], "height" => getimagesize($src)[1]);
+	
+	for($h = 0; $h < 50; $h++){
+	for($w = 0; $w < $img_size["width"]; $w++){
+		$rgb = imagecolorat($img, $w, $h);
+		$r = ($rgb >> 16) & 0xFF;
+		$g = ($rgb >> 8) & 0xFF;
+		$b = $rgb & 0xFF;
+
+		echo '<div class="p" style="background:rgb('.$r.', '.$g.', '.$b.');"></div>';
+	}
+		echo "<br />";
+	}
+
+	
 ?>
